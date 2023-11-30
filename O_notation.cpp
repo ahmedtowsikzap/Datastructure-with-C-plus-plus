8aad0fc974f3a17905d2f0334b49dcfe9910a2d4
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-12-01 01:07:59 +0600

    O_notation part

--- O_notation.cpp
+++ O_notation.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 int main(){
     
-    printf("HELLO");
- 
+  int n; // step 1
+
+  cin>>n; //step 1
+
+  int a[n]; // step-1
+
+  for(int i=0; i<n; i++){ // n step o(n)
+
+    cin>>a[i];
+  } 
+
+  int s=0; // step1 
+  for(int i=0; i<n; i++){ //n step  o(n)
+        
+         s = s+ a[i];
+    } 
+
+  cout<<s<<endl;
     return 0;
 
+
+
+//Total  O(n) + o(n) = o(2n) = o(n) and thats it
 }
