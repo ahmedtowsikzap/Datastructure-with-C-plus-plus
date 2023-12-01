4c2a8e3f6fa859cbf688adb9242c4ed8eba2a7bd
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-12-01 12:42:58 +0600

    vector size and vector initilization using stl and its usecases on how it works and many more!

--- vector.cpp
+++ vector.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main(){
     
     // vector<int> v; type 1 only size o(1);
-
-    vector<int> v(5, 10);
-
+    //vector<int> v(5); automatically declare the size of it which is 5 in this case!!
+    // vector<int> v(5, 10); It's 5 size array but in every position it has the value of 10 in it!!!!
+    vector<int> v2(5,100);
+     vector<int> v(v2);
     for(int i=0; i<v.size(); i++){
 
         cout<<v[i]<<" ";
