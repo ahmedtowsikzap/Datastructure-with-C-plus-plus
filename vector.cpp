f3ce294712a5f02cada2e5e4b828407ada593785
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2023-12-01 12:31:36 +0600

    vector type 1 and 2

--- vector.cpp
+++ vector.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main(){
     
-    vector<int> v;
+    // vector<int> v; type 1 only size o(1);
+
+    vector<int> v(5, 10);
+
+    for(int i=0; i<v.size(); i++){
+
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
     cout<<v.size();
 
     return 0;
